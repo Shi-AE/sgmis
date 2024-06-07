0372b996d44dbb128eed2258bdffd68589faf44a
A.E. <570796406@qq.com>
2024-06-07 22:18:14 +0800

    Android登录信息模块

--- Android/app/src/main/java/com/example/sgmis_java/api/service/LoginService.java
+++ Android/app/src/main/java/com/example/sgmis_java/api/service/LoginService.java
@@ -1,12 +1,16 @@
 package com.example.sgmis_java.api.service;
 
 import com.example.sgmis_java.domain.dto.UserDTO;
+import com.example.sgmis_java.domain.pojo.LoginMsg;
 import com.example.sgmis_java.domain.pojo.Result;
 import com.example.sgmis_java.domain.vo.UpdateUserVo;
 import com.example.sgmis_java.domain.vo.UserVo;
 
+import java.util.List;
+
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 
@@ -17,4 +21,7 @@ public interface LoginService {
 
     @PUT("login")
     Observable<Result<String>> updatePassword(@Body UpdateUserVo userVo);
+
+    @GET("login/message")
+    Observable<Result<List<LoginMsg>>> getLoginMessage();
 }
--- Android/app/src/main/java/com/example/sgmis_java/view/LoginInfoActivity.java
+++ Android/app/src/main/java/com/example/sgmis_java/view/LoginInfoActivity.java
@@ -1,8 +1,15 @@
 package com.example.sgmis_java.view;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,6 +17,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.sgmis_java.R;
+import com.example.sgmis_java.api.Api;
+import com.example.sgmis_java.api.service.LoginService;
+import com.example.sgmis_java.domain.pojo.LoginMsg;
+
+import java.time.format.DateTimeFormatter;
 
 public class LoginInfoActivity extends AppCompatActivity {
     @Override
@@ -22,5 +34,43 @@ public class LoginInfoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ListView loginList = findViewById(R.id.loginList);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(Api.yyyyMMddHHmmss);
+        Api.execute(this, Api.creat(LoginService.class).getLoginMessage(), (data, msg) -> {
+            loginList.setAdapter(new ArrayAdapter<LoginMsg>(getApplication(), R.layout.login_item, data) {
+                @NonNull
+                @Override
+                public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+                    if (convertView == null) {
+                        convertView = LayoutInflater.from(getContext()).inflate(R.layout.login_item, parent, false);
+                    }
+
+                    LoginMsg item = getItem(position);
+
+                    if (item == null) {
+                        return LayoutInflater.from(getContext()).inflate(R.layout.login_item, parent, false);
+                    }
+
+                    TextView accountText = convertView.findViewById(R.id.account);
+                    TextView ipText = convertView.findViewById(R.id.ip);
+                    TextView timeText = convertView.findViewById(R.id.time);
+                    TextView browserText = convertView.findViewById(R.id.browser);
+                    TextView osText = convertView.findViewById(R.id.os);
+                    TextView deviceText = convertView.findViewById(R.id.device);
+
+                    accountText.setText(item.getAccount());
+                    ipText.setText(item.getIp());
+                    timeText.setText(item.getTime().format(formatter));
+                    browserText.setText(item.getBrowser());
+                    osText.setText(item.getOs());
+                    deviceText.setText(item.getDevice());
+
+                    return convertView;
+                }
+            });
+        });
+
     }
 }
